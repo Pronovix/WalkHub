ffc0a2f5bde5f45c6f66366a8dca0621f455df65
tokapeb <tokap@gmail.com>
2013-04-04 01:28:40 +0200

    theme info with region confs, page tpl(unfinished)

---
+++ sites/all/themes/walk_theme/templates/page--other.tpl.php
@@ -0,0 +1,3 @@
+<div class="other">
+    <p>kaka</p>
+</div>
--- sites/all/themes/walk_theme/templates/page_OLD_.tpl.php
+++ sites/all/themes/walk_theme/templates/page_OLD_.tpl.php
@@ -8,34 +8,36 @@
         <span class="icon-bar"></span>
       </a>
 
-      <div id="logo-region" class="span4">
-        <?php if (!empty($logo)): ?>
-          <!-- Logo region -->
-          <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-            <img src="/<?php print $GLOBALS['theme_path'] ?>/images/walkhub-logo-light.svg" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
-      </div>
-
-      <?php if (!empty($page['header_menu'])): ?>
-        <!-- Header menu region -->
-        <div id="header-menu-region" class="span6">
-          <?php print render($page['header_menu']); ?>
-        </div>
+      <?php if (!empty($logo)): ?>
+        <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="/<?php print $GLOBALS['theme_path'] ?>/images/walkhub-logo-light.svg" alt="<?php print t('Home'); ?>" />
+        </a>
       <?php endif; ?>
 
-      <?php if (!empty($page['search'])): ?>
-        <!-- Search region -->
-        <div id="search-region" class="span2">
-          <?php print render($page['search']); ?>
-        </div>
+      <?php if (!empty($site_name)): ?>
+        <h1 id="site-name">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+        </h1>
       <?php endif; ?>
 
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="nav-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </header>
-
-
 
 <div class="main-container container">
 
