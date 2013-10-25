1938c32ede69707af0ce01daba7f4c22b2e95333
Peter Marosi <peter.marosi@pronovix.com>
2013-10-25 10:59:43 +0200

    Sidebar base

--- sites/all/themes/walkhub/template.php
+++ sites/all/themes/walkhub/template.php
@@ -177,6 +177,7 @@ function walkhub_css_alter(&$css) {
   // Remove system css
   unset($css[drupal_get_path('module','system').'/system.menus.css']);
   unset($css[drupal_get_path('module','field_collection').'/field_collection.theme.css']);
+  unset($css[drupal_get_path('module','system').'/system.theme.css']);
 }
 
 /**
