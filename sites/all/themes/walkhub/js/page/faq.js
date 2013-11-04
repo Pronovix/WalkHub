ec2c6ae8238173330b3d0a51035fd2c35a186c42
Peter Marosi <peter.marosi@pronovix.com>
2013-11-04 15:27:47 +0100

    Fix for the faq

--- sites/all/themes/walkhub/js/page/faq.js
+++ sites/all/themes/walkhub/js/page/faq.js
@@ -4,7 +4,6 @@
     attach: function (context, settings) {
       var allAns = $('.field-collection-item-field-walkhub-questions .field-name-field-walkhub-faq-answer').hide();
       var allQuest = $('.field-collection-item-field-walkhub-questions .field-name-field-walkhub-faq-questions');
-      console.log('processed');
       $('.field-collection-item-field-walkhub-questions .field-name-field-walkhub-faq-questions').click(function() {
         allAns.slideUp();
         allQuest.removeClass("active");
