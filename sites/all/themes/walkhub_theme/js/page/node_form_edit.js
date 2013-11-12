31c17e07c8a03ad9fe10ca3a33bd820d4c94c415
Peter Marosi <peter.marosi@pronovix.com>
2013-11-12 18:18:21 +0100

    Codes

--- sites/all/themes/walkhub_theme/js/page/node_form_edit.js
+++ sites/all/themes/walkhub_theme/js/page/node_form_edit.js
@@ -11,13 +11,13 @@
     attach: function (context, settings) {
       Drupal.walkhubStepEdit.makeButton(context, settings);
       Drupal.walkhubStepEdit.makeBackButton(context, settings);
+      Drupal.walkhubStepEdit.insertIcon(context, settings);
     }
   };
 
   Drupal.walkhubStepEdit = {
     makeButton : function(context, settings) {
-      $('#ad-set .le')
-        .append('<a class="button small open"><i class="icon-cog"></i> Advanced Settings</a>')
+      $('#ad-set .le').append('<a class="button small open"><i class="icon-cog"></i> Advanced Settings</a>')
         .click(function(){
           $("#ad-set .ri").toggle( "200" );
           return false;
@@ -25,6 +25,14 @@
     },
     makeBackButton : function(context, settings) {
       $('#edit-submit').before('<a class="back button" href="javascript: history.go(-1)"><i class="icon-chevron-left"></i> Back</a>')
+    },
+
+    insertIcon : function(context, settings) {
+      $('#edit-delete').prepend('<i class="icon-trash"></i> ');
+      $('#edit-submit').prepend('<i class="icon-upload"></i> ');
+      $('#edit-preview').prepend('<i class="icon-browser"></i> ');
+      $('#edit-preview-changes').prepend('<i class="icon-eye"></i> ');
+      $('#edit-field-fc-steps-und-add-more--2').prepend('<i class="icon-plus"></i> ');
     }
   }
 
