1b396c76b1bc6c70842a8c1405b6ee522ba88cf0
Attila Cs.Nagy <cs.nagy.attila@gmail.com>
2013-11-13 12:40:44 +0100

    fixed that the walkthrough edit form initialization methods only run once

--- sites/all/themes/walkhub_theme/js/page/node_form_edit.js
+++ sites/all/themes/walkhub_theme/js/page/node_form_edit.js
@@ -21,27 +21,53 @@
 
   Drupal.walkhubStepEdit = {
     makeButton : function(context, settings) {
-      $('#ad-set .le').append('<a class="button small open"><i class="icon-cog"></i> Advanced Settings</a>')
-        .click(function(){
-          $("#ad-set .ri").toggle( "200" );
-          return false;
-        });
-    },
-    makeBackButton : function(context, settings) {
-      $('#edit-submit').before('<a class="back button" href="javascript: history.go(-1)"><i class="icon-chevron-left"></i> Back</a>')
+      $('#ad-set .le', context).once('makeButton', function() {
+        var $this = $(this);
+        $this.append('<a class="button small open"><i class="icon-cog"></i>' + Drupal.t("Advanced Settings") + '</a>').click(function() {
+            $("#ad-set .ri").toggle( "200" );
+            return false;
+          });
+      })
     },
 
+    makeBackButton : function(context, settings) {
+      $('#edit-submit', context).once('makeBackButton', function() {
+        var $this = $(this);
+        $this.before('<a class="back button" href="javascript: history.go(-1)"><i class="icon-chevron-left"></i>' + Drupal.t("Back") + '</a>');
+      });
+    },
+
+
     insertIcon : function(context, settings) {
-      $('#edit-delete').prepend('<i class="icon-trash"></i> ');
-      $('#edit-submit').prepend('<i class="icon-upload"></i> ');
-      $('#edit-preview').prepend('<i class="icon-browser"></i> ');
-      $('#edit-preview-changes').prepend('<i class="icon-eye"></i> ');
-      $('#edit-field-fc-steps-und-add-more--2').prepend('<i class="icon-plus"></i> ');
+      $('#edit-delete', context).once('delete-button', function() {
+        var $this = $(this);
+        $this.prepend('<i class="icon-trash"></i> ');
+      });
+
+      $('#edit-submit', context).once('submit-button', function() {
+        var $this = $(this);
+        $this.prepend('<i class="icon-upload"></i> ');
+      });
+
+      $('#edit-preview', context).once('preview-button', function() {
+        var $this = $(this);
+        $this.prepend('<i class="icon-browser"></i> ');
+      });
+
+      $('#edit-preview-changes', context).once('preview-changes-button', function() {
+        var $this = $(this);
+        $this.prepend('<i class="icon-eye"></i> ');
+      });
+
+      $('#edit-field-fc-steps-und-add-more--2', context).once('field-fc-steps-und-add-more--2-button', function() {
+        var $this = $(this);
+        $this.prepend('<i class="icon-plus"></i> ');
+      });
     },
 
 
     stepHideShow : function(context, settings) {
-      $('.step-title.button').click(function(){
+      $('.step-title.button', context).click(function() {
           $('.walkthrough-step-container').toggle( "200" );
           return false;
         });
