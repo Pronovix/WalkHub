22180adb30cf6bf2d1fda6cdfb628243f729f36b
Kornel Lugosi <coornail@gmail.com>
2014-04-24 21:41:25 +0100

    Build theme styles with gulp

---
+++ sites/all/themes/walkhub_theme/gulpfile.js
@@ -0,0 +1,56 @@
+(function () {
+  "use strict";
+
+  var gulp = require('gulp');
+
+  var args = require('yargs').argv;
+  var clean = require('gulp-clean');
+  var uglify = require('gulp-uglify');
+  var concat = require('gulp-concat');
+  var gulpif = require('gulp-if');
+  var compass = require('gulp-compass');
+  var plumber = require('gulp-plumber');
+  var csso = require('gulp-csso');
+  var cmq = require('gulp-combine-media-queries');
+
+  var paths = {
+    sass: ["sass/custom.sass"],
+  };
+
+
+  gulp.task('buildsass', function () {
+    var sassConfig = {
+      css: '.tmp',
+      sass: 'sass',
+    };
+
+    if (args.debug) {
+      sassConfig.style = 'expanded';
+    }
+
+    return gulp.src(paths.sass)
+      .pipe(plumber())
+      .pipe(compass(sassConfig))
+      .pipe(gulp.dest('.tmp'));
+  });
+
+  // Processing css must happen in a separate step:
+  // https://github.com/appleboy/gulp-compass/issues/2
+  gulp.task('process-css', function() {
+    gulp.src(['.tmp/*.css'])
+      .pipe(cmq({log: true}))
+      .pipe(gulp.dest('css'))
+      .pipe(csso())
+      .pipe(gulp.dest('css'));
+  });
+
+  gulp.task('build', ['buildsass']);
+  gulp.watch('.tmp/*.css', function(){gulp.run('process-css');});
+
+  gulp.task('clean', function () {
+    return gulp.src(['walkthrough.css'])
+      .pipe(clean());
+  });
+
+  gulp.task('default', ['build']);
+})();
