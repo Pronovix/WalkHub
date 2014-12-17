a7771c97d66b6c7aed19ff0fd7edbedf3e6bce43
Ricsi Molnar <erramgeor@gmail.com>
2014-12-17 12:03:15 +0100

    Pr fix

--- sites/all/modules/features/walkthrough_global/login_button.tpl.php
+++ sites/all/modules/features/walkthrough_global/login_button.tpl.php
@@ -1,16 +1,7 @@
-<?php
-if (user_is_logged_in()) {
-  return [];
-}else{
-  ?>
 <div class="login-container">
   <div class="row">
     <div><a href="/user">Login</a></div>
   </div>
 </div>
-<?php
-}
-
-?>
 
 
