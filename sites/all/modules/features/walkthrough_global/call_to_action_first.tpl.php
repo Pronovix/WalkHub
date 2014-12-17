fc127207aa963be1dab0c35f0bafdc572664fd8f
Ricsi Molnar <erramgeor@gmail.com>
2014-12-17 13:47:38 +0100

    Pr fix

--- sites/all/modules/features/walkthrough_global/call_to_action_first.tpl.php
+++ sites/all/modules/features/walkthrough_global/call_to_action_first.tpl.php
@@ -1,10 +1,7 @@
 <div class="row">
   <div class="large-12 columns">
-    <section class="block block-block block-block-15">
-
-
       <section class="reinforcement">
         <h6 class="slogan center"> Choose the WalkHub hosted version if you don't want to think about server maintenance, updates and storage.  <br>Choose the WalkHub On-Prem edition to set up your own fully functional WalkHub. </h6>
       </section>
-    </section>      </div>
+  </div>
 </div>
